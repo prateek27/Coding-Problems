dfc80ff0565b9df4eba3df46d6e6fa6474e37779
Prateek Narang <prateekrishu@yahoo.com>
2015-01-12 23:35:08 +0530

    oops templates

--- OOPS and templates/2. Linked List Template.cpp
+++ OOPS and templates/2. Linked List Template.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 template<typename T>
 class Node{
+public:
 T obj;
 Node*next;
-public:
-    Node(const T &obj){
-        this->obj = obj;
+
+    Node(const T &ob): obj(ob){
+        //this->obj = obj;
     }
     T getObject(){
         return obj;
@@ -22,6 +23,19 @@ public:
     }
 };
 
+
+class Person{
+int data;
+int marks;
+public:
+    Person(int a,int b){
+        data = a;
+        marks = b;
+    }
+    int getData(){
+    return data;
+    }
+};
 
 template<typename T>
 class LinkedList{
@@ -56,13 +70,8 @@ public:
             }
         }
 
-    friend ostream& operator<<(ostream out,LinkedList<T> L);
 
-};
-
-
-    template<typename T>
-    ostream& operator<<(ostream out,LinkedList<T> L){
+    friend ostream& operator<<(ostream &out,LinkedList<T> &L) {
     while(L.head!=NULL){
         cout<<L.head->getObject().getData()<<" -> ";
         L.head = L.head->getNext();
@@ -71,19 +80,16 @@ public:
     return out;
     }
 
-
-class Person{
-int data;
-int marks;
-public:
-    Person(int a,int b){
-        data = a;
-        marks = b;
-    }
-    int getData(){
-    return data;
-    }
 };
+
+
+    /*template<typename T>
+    friend ostream& operator<<(ostream &out, LinkedList<T> &L){
+
+    }*/
+
+
+
 
 int main(){
 LinkedList<Person> L;
@@ -92,6 +98,15 @@ Person p2(2,20);
 Person p3(3,30);
 Person p4(4,40);
 Person p5(5,50);
-
+L.insertBack(p1);
+L.insertBack(p2);
+L.insertBack(p3);
+L.insertBack(p4);
+L.insertBack(p5);
+L.insertBack(p5);
+LinkedList<int> A;
+A.insertBack(5);
+A.insertBack(6);
+//cout<<A;
 cout<<L;
 }
