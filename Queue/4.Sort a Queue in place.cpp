66385b829c4dad57f03be11cd9488be1b0f9dd0b
Prateek Narang <prateekrishu@yahoo.com>
2015-07-28 23:57:01 +0530

    Update 4.Sort a Queue in place.cpp

--- Queue/4.Sort a Queue in place.cpp
+++ Queue/4.Sort a Queue in place.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 queue<int> q;
-
+//This function just returns the minimum in the window 0 to k
 int findMinimum(int k,int n)
 { int min = 100000;
 for(int i=0;i<n;i++){
@@ -16,7 +16,7 @@ for(int i=0;i<n;i++){
     }
     return min;
 }
-
+//This places the minimum value at correct position
 void reorder(int min,int n){
 for(int i=0;i<n;i++){
     int current = q.front();
